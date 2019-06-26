b0cf7083245e23f6d352761c08a5223de03f3dab
Keatra Nesbitt <keatralynn@gmail.com>
2019-06-26 18:16:22 -0500

    able to log in using program

--- webscrape.py
+++ webscrape.py
@@ -9,15 +9,23 @@ driver = webdriver.Chrome('/Users/keatra/Downloads/chromedriver')
 
 #%%
 
-def IG_login(url, username, password):
+def IG_login(url, my_username_, my_password):
     driver.get(url)
-    driver.find_element_by_name(“username”).send_keys(username)
-    driver.find_element_by_name(“password”).send_keys(password)
-    driver.find_element_by_name(“submit”).click()
+    driver.find_element_by_name('username').send_keys(my_username)
+    driver.find_element_by_name('password').send_keys(my_password)
+    driver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
 
 
 #%%
-url = 'https://www.instagram.com/'
-username = '_knesbitt'
-password = '2016riley'
-IG_login(url, username, password)
+url = 'https://www.instagram.com/accounts/login'
+my_username = '_knesbitt'
+my_password = '2016riley'
+IG_login(url, my_username, my_password)
+
+#%%
+driver.find_element_by_partial_link_text("a href='/_knesbitt/'").click()
+
+#%%
+
+
+#%%
