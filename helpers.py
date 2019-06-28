2d3c16fa31f323262e86260482704010b6121c5f
Keatra Nesbitt <keatralynn@gmail.com>
2019-06-28 09:49:21 -0500

    added wait time function

--- helpers.py
+++ helpers.py
@@ -1,8 +1,39 @@
+import numpy as np 
+import pandas as pd 
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome('/Users/keatra/Downloads/chromedriver')
 
-def IG_login(url, my_username, my_password):
+
+def expo_wait():
+    '''
+    expo wait - create an exponential random sleep time
+    '''
+    time.sleep(210*np.random.exponential())
+
+
+def main(url, my_username, my_password):
+    #go to instagram login page 
     driver.get(url)
+    time.sleep(3)
+
+    #enter username and passwords
     driver.find_element_by_name('username').send_keys(my_username)
     driver.find_element_by_name('password').send_keys(my_password)
+    time.sleep(3)
+
+    #Login
     driver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
+    time.sleep(3)
+
+    #go to profile page
+
+    #get total number of post and total number of followers
+
+
+    #scrape page for photos, datetime stamp, and caption
+
+    #end session
+    driver.close()
+
--- webscrape.py
+++ webscrape.py
@@ -1,16 +1,13 @@
 
-#%%
-from selenium import webdriver
-import pandas as pd 
-import numpy as np 
-from helpers import IG_login
+#%% 
+from helpers import main
 #%%
 u = open('/Users/keatra/.ssh/IG_username.txt', 'r')
 p = open('/Users/keatra/.ssh/IG_password.txt', 'r')
 url = 'https://www.instagram.com/accounts/login'
 my_username = u.read().strip('\n')
 my_password = p.read().strip('\n')
-IG_login(url, my_username, my_password)
+main(url, my_username, my_password)
 u.close()
 p.close()
 #%%
