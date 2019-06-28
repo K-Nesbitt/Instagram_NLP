94622eb79ab498352e8646a1ba70daefa389b24f
Keatra Nesbitt <keatralynn@gmail.com>
2019-06-28 17:00:04 -0500

    successful scraped 11 images -1 video

--- helpers.py
+++ helpers.py
@@ -35,20 +35,54 @@ def main(webdriver, url, my_username, my_password):
 
     post_element = [x for x in list_elements if x.text.find('posts') != -1] 
     total_posts = post_element[0].text
-    print(total_posts)    
-
+    num_posts = int(post_element[0].text.split(' ')[0])    
+    pages = round(num_posts/12, 0)   
     followers_element = [x for x in list_elements if x.text.find('followers') != -1]
     total_followers = followers_element[0].text
-    print(total_followers)
     
-    following_element = [x for x in list_elements if x.text.find('following') != -1]
-    total_following = following_element[0].text    
+    
+    '''following_element = [x for x in list_elements if x.text.find('following') != -1]
+    total_following = following_element[0].text  '''  
 
-    #scrape page for photos, datetime stamp, and caption
+    #scrape page for information on each picture
+    picture_info = []
+    element = webdriver.find_element_by_tag_name('article')
+    for _ in range(pages):
+        all_links = webdriver.find_elements_by_tag_name('a')   
+        for link in all_links:
+                picture_link = link.get_attribute('href') 
+                if picture_link.find('/p/') != -1:
+                        # Open new tab
+                        webdriver.execute_script("window.open('');")
+                        time.sleep(3)
 
+                        # Switch to the new window
+                        webdriver.switch_to.window(webdriver.window_handles[1])
+                        webdriver.get(picture_link)
+                        time.sleep(5)
+                        likes_list = webdriver.find_elements_by_class_name('zV_Nj')
+                        if len(likes_list) != 0:
+                                num_likes = int(likes_list[1].text.split(' ')[0]) + 1
+
+                                title = webdriver.find_element_by_class_name('_6lAjh').text
+                                if title == my_username:
+                                        caption_list = webdriver.find_elements_by_xpath("//div[@class='C4VMK']//span")
+                                        '''num_of_comments = len(caption_list)'''
+                                        caption = caption_list[0].text
+                                else:
+                                        caption = 'none'
+                                picture_info.append([num_likes, caption])
+                
+                        webdriver.close()
+                        
+                        # Switch focus back to main tab
+                        webdriver.switch_to.window(webdriver.window_handles[0])    
+                        time.sleep(5) 
+        element.send_keys(Keys.END)
+        time.sleep(2)       
     #end session
-    #webdriver.close()
-    return None
+    webdriver.close()
+    return (total_posts, total_followers, picture_info)
 
 if __name__ == "__main__":
         u = open('/Users/keatra/.ssh/IG_username.txt', 'r')
@@ -59,4 +93,5 @@ if __name__ == "__main__":
         p.close()
         url = 'https://www.instagram.com/accounts/login'
         driver = webdriver.Chrome('/Users/keatra/Downloads/chromedriver')
-        main(driver,url, my_username, my_password)
+        (n, f, pics) = main(driver,url, my_username, my_password)
+
