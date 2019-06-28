063ef69f2501b904eaa0b510afd57467c04753a7
Keatra Nesbitt <keatralynn@gmail.com>
2019-06-28 10:45:43 -0500

    able to get to profile page

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome('/Users/keatra/Downloads/chromedriver')
+
 
 
 def expo_wait():
@@ -13,21 +13,23 @@ def expo_wait():
     time.sleep(210*np.random.exponential())
 
 
-def main(url, my_username, my_password):
+def main(webdriver, url, my_username, my_password):
     #go to instagram login page 
-    driver.get(url)
+    webdriver.get(url)
     time.sleep(3)
 
     #enter username and passwords
-    driver.find_element_by_name('username').send_keys(my_username)
-    driver.find_element_by_name('password').send_keys(my_password)
+    webdriver.find_element_by_name('username').send_keys(my_username)
+    webdriver.find_element_by_name('password').send_keys(my_password)
     time.sleep(3)
 
     #Login
-    driver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
+    webdriver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
+    time.sleep(5)
+    webdriver.find_element_by_xpath("//*[contains(text(), 'Not Now')]").click()
     time.sleep(3)
-
     #go to profile page
+    webdriver.find_element_by_css_selector("a[href*='/_knesbitt/']").click()
 
     #get total number of post and total number of followers
 
@@ -35,5 +37,5 @@ def main(url, my_username, my_password):
     #scrape page for photos, datetime stamp, and caption
 
     #end session
-    driver.close()
+    #webdriver.close()
 
--- webscrape.py
+++ webscrape.py
@@ -1,22 +1,27 @@
-
+#%%
+%load_ext autoreload
+%autoreload 2
 #%% 
 from helpers import main
+from selenium import webdriver
 #%%
 u = open('/Users/keatra/.ssh/IG_username.txt', 'r')
 p = open('/Users/keatra/.ssh/IG_password.txt', 'r')
-url = 'https://www.instagram.com/accounts/login'
 my_username = u.read().strip('\n')
 my_password = p.read().strip('\n')
-main(url, my_username, my_password)
 u.close()
 p.close()
-#%%
-driver.find_element_by_partial_link_text("a href='/_knesbitt/'").click()
+url = 'https://www.instagram.com/accounts/login'
+#%% 
+driver = webdriver.Chrome('/Users/keatra/Downloads/chromedriver')
+main(driver,url, my_username, my_password)
 
 #%%
+driver.find_element_by_partial_link_text(href='/_knesbitt/').click()
+#%%
+driver.find_element_by_css_selector("a[href*='/_knesbitt/']")).click()
+
+
 
 
 #%%
-
-
-
