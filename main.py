e132faba1c3563b8da1447c72dca9e515d741ca4
Keatra Nesbitt <keatralynn@gmail.com>
2019-06-30 09:59:29 -0500

    create pd and save in loop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from functions import login, totals, get_picture_links, scrape_page
 import pandas as pd 
 import time
-
+import csv 
 #%%
 u = open('/Users/keatra/.ssh/IG_username.txt', 'r')
 p = open('/Users/keatra/.ssh/IG_password.txt', 'r')
@@ -36,20 +36,23 @@ IGdriver = login(my_username, my_password)
 
 users_dict = {}
 for user in users:
-    time.sleep(10)
+    time.sleep(5)
     IGdriver.get('https://www.instagram.com/{}/'.format(user))
+    time.sleep(5)
     user_posts, user_followers = totals(IGdriver)
     time.sleep(3)
     user_links = get_picture_links(IGdriver, user_posts)
-    time.sleep(5)
+    time.sleep(7)
     user_info = scrape_page(IGdriver, user_links, user)
     users_dict[user] = [user_posts, user_followers, user_info]
 
+    df = pd.DataFrame(user_info, columns=['number_of_likes', 'caption'])
+    df.to_csv(path_or_buf='/Users/keatra/Galvanize/Projects/Instagram_likes_nlp/data/{}.csv'.format(user))
+    
 
 #%%
-
-
-#%%
-
+w = csv.writer(open("/Users/keatra/Galvanize/Projects/Instagram_likes_nlp/data/{}.csv".format(user), "w"))
+    for key, val in users_dict.items():
+        w.writerow([key, val])
 
 #%%
