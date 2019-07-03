0cc27acac4de03848b5d13d5b6905ebc11d01ae0
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-03 14:56:13 -0500

    moved py files into folder. cleaned scraping.py

--- python functions/scraping.py
+++ python functions/scraping.py
@@ -3,23 +3,19 @@ import pandas as pd
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import glob
-
-def expo_wait():
-    '''
-    expo wait - create an exponential random sleep time
-    '''
-    time.sleep(210*np.random.exponential())
 
 
 def login(my_username, my_password):
-        '''This function will initiate a webdriver and
-         log in into Instagram 
-        with the given username and password and 
-        direct you to the profile page. '''
+        '''This function will initiate a webdriver, log into Instagram,
+        and go to the user's profile page. 
+
+        Parameters: Instagram username and password 
+        
+        Returns:
+        The driver object'''
 
         url = 'https://www.instagram.com/accounts/login'
-        driver = webdriver.Chrome('/Users/keatra/Downloads/chromedriver')
+        driver = webdriver.Chrome('/Users/keatra/Galvanize/chromedriver')
         
         #go to instagram login page 
         driver.get(url)
@@ -41,8 +37,12 @@ def login(my_username, my_password):
         return driver
 
 def totals(webdriver):
-        '''This function will find and return the total 
-        number of posts and followers on a page'''
+        '''This function will get the total number of posts, followers, 
+        and following for what ever page the webdriver is on. 
+
+        Parameters: the webdrive that is active
+
+        Returns: tuple of the total posts and total followers.'''
 
         list_elements = webdriver.find_elements_by_tag_name('li')
 
@@ -52,18 +52,27 @@ def totals(webdriver):
         followers_element = [x for x in list_elements if x.text.find('followers') != -1]
         total_followers = followers_element[0].text
 
-        '''following_element = [x for x in list_elements if x.text.find('following') != -1]
-        total_following = following_element[0].text  '''  
+        #I commented this code out because it was not useful in my data but I still
+        # wanted to keep the code if I ever decided to use it. 
+        #following_element = [x for x in list_elements if x.text.find('following') != -1]
+        #total_following = following_element[0].text   
 
         return total_posts, total_followers
 
 
 def get_picture_links(webdriver, total_posts):
         '''This function will find all links that have
-        pictures on the page'''
+        pictures on the page
+
+        Parameters: the active webdriver, the total posts on that page
+
+        Returns: a set of links to pictures and videos
+        '''
         posts = total_posts.split(' ')[0].replace(',', '')
         num_posts = int(posts)   
-        pages = (num_posts//12)
+        pages = (num_posts//12) #we divide by 12 because that is the average number of displayed
+                                #photos by page
+        
         link_set = set()
         html = webdriver.find_element_by_tag_name('html')
 
@@ -83,7 +92,13 @@ def scrape_page(webdriver, links, username):
         '''This function will go to all links provided
         and scrape each picture for the number of likes
         and the caption. It will only provide the caption if the 
-        identified user is the title'''
+        identified user is the title
+        
+        Parameters: the active webdriver, a set of picture links, 
+        the username of the page your are scraping
+
+        Returns: a list of list with the number of likes and caption
+        '''
         picture_info = []
 
         for link in links:
@@ -97,7 +112,9 @@ def scrape_page(webdriver, links, username):
                 time.sleep(5)
                 try:
                         likes_list = webdriver.find_elements_by_class_name('zV_Nj')
-                        if len(likes_list) != 0:
+
+                        if len(likes_list) != 0: #If the length is 0, then it is a video
+                                #!error in data
                                 if len(likes_list) == 1:
                                         num_likes = 5
                                 
@@ -108,7 +125,8 @@ def scrape_page(webdriver, links, username):
                                         title = webdriver.find_element_by_class_name('_6lAjh').text
                                         if title == username:
                                                 caption_list = webdriver.find_elements_by_xpath("//div[@class='C4VMK']//span")
-                                                '''num_of_comments = len(caption_list)'''
+                                                #This code works but not active since I did not use the information
+                                                # num_of_comments = len(caption_list)
                                                 caption = caption_list[0].text
                                         else:
                                                 caption = None
@@ -118,7 +136,7 @@ def scrape_page(webdriver, links, username):
 
                                 picture_info.append([num_likes, caption])
                 except:
-                        num_likes = None 
+                        pass
                 webdriver.close()
                 
                 # Switch focus back to main tab
@@ -127,7 +145,15 @@ def scrape_page(webdriver, links, username):
        
         return picture_info
  
-def users_scrape_save(my_username, my_password, users, df_columns=['number_of_likes', 'caption']):
+def users_scrape_save(my_username, my_password, users):
+        '''This function will scrape a given set of users pages and 
+        save the information by page into a csv in the data folder of the directory. 
+
+        Parameters: Instagram username and password, list of usernames for other people,
+        
+        Returns: None. Files are saved as the program is running. '''
+
+        #Assumes original driver was closed and will log in to Instagram
         IGdriver = login(my_username, my_password)
 
         for user in users:
@@ -140,7 +166,7 @@ def users_scrape_save(my_username, my_password, users, df_columns=['number_of_li
                 time.sleep(7)
                 user_info = scrape_page(IGdriver, user_links, user)
 
-                df = pd.DataFrame(user_info, columns= df_columns)
+                df = pd.DataFrame(user_info, columns=['number_of_likes', 'caption'])
                 df.to_csv(path_or_buf= '/Users/keatra/Galvanize/Projects/Instagram_likes_nlp/data/{}.csv'.format(user))
         return None
 
