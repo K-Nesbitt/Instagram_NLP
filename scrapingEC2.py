fe6c2ccf86d93898e3bfa403ddd5a1c0e8a589c6
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-16 17:45:16 -0500

    added keys to import

--- scrapingEC2.py
+++ scrapingEC2.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
