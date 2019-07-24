323d953ef17c2660282e087c7277ee6f890158e6
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-24 18:04:42 -0500

    reorganized code

--- src/new_work.py
+++ src/new_work.py
@@ -25,20 +25,39 @@ df_raw = csvs_to_df(data_path)
 df_raw.head()
 
 #%%
-#Plot the number of likes on a histogram
+#Turn caption column into list of tokenized words
+#Add column to count number of words in caption
+likes_caption_df  = clean_text(df_raw)
+full_df = add_word_count(likes_caption_df)
+full_df.head()
+
+#%%
+#Graph a histogram of the  number of likes 
 num_likes = hv.Histogram(np.histogram(df_raw['number_of_likes'], 250))
 num_likes.opts(xlabel='Number of Likes', xticks=10)
 num_likes.redim(x=hv.Dimension('x', range=(0, 200)))
 
+#%%
+#Graph relationship between number of likes and number of words
+likes = full_df['number_of_likes'].values
+words = full_df['number_of_words'].values
+
+words_likes = hv.Scatter((words, likes)).opts(xlabel='Number of words in caption', ylabel='Number of likes',title='Words vs. Likes')
+words_likes.redim(x=hv.Dimension('x', range=(0, 100)), y=hv.Dimension('y', range=(0,100)))
 
 #%%
+#Graph a histogram of the number of words per document
+hv.Histogram(np.histogram(full_df['number_of_words'].values)).opts(xlabel='Number of Words in caption')
+
+#%%
+#Collect the total post and number of likes for each user. 
 users= [ 'adizz82', 'blake.kelch', 'briannanmoore13', 'caseybarnold', 'cclay2', 'copperhead_etx', 'faithandfuel',
                 'fitness_with_mercy', 'fresco5280', 'happy_hollydays_', 'jhousesrt8','_knesbitt', 'mckensiejoo', 
                 'oletheamclachlan', 'phensworld', 'richardrobinsonmusic', 'sirlawrencecharles', 'keilam7', 'dr_kerrie', 
                 'pina.risa', 'presmith', 'giftedhands_crochet_and_crafts','jeffersonmason4', 'dmdanamitchell', 
                 'suntanned_superman_', 'laceycooley', 'goulding_jr']
 user_totals = user_totals(users)
-#%%
+
 user_totals['number_of_followers'] = user_totals['number_of_followers'].apply(lambda x: x.replace('.', '') if type(x)==str else x)
 user_totals['number_of_followers'] = user_totals['number_of_followers'].apply(lambda x: x.replace('k', '000') if type(x)==str else x)
 user_totals['number_of_followers']= user_totals['number_of_followers'].astype(int)
@@ -47,6 +66,7 @@ user_totals.describe()
 #%%
 drop_high_users = user_totals.drop(labels=['jeffersonmason4', 'dmdanamitchell'])
 drop_high_users.describe()
+
 #%%
 #Plot the number of posts
 num_posts = hv.Histogram(np.histogram(user_totals['number_of_posts'], 50))
@@ -62,12 +82,6 @@ num_followers.opts(xlabel='Number of Followers per User', xticks=5)
 num_followers.redim(x=hv.Dimension('x', range=(0, 1200)))
 
 #%%
-#This function will lowercase and remove special characters from the caption 
-likes_caption_df  = clean_text(df_raw)
-full_df = add_word_count(likes_caption_df)
-full_df.head()
-
-#%%
 #Create a corpus from the rows in a dataframe
 corpus = create_corpus(full_df)
 word_corpus = tokenize_corpus(corpus)
@@ -79,22 +93,22 @@ words = Counter(word_corpus)
 sorted_words = sorted(words.items(), key=operator.itemgetter(1))
 
 #%%
+#Graph the frequency of the top ten words
 frequency = hv.Scatter(sorted_words[-10:])
 frequency.opts(size=7, title='Word Frequency', xlabel='Word', ylabel='Number of Times in Corpus')
 #%%
-#create a train and test set of data
+#create a train and test set of data (corpus of words and number of words)
 X = pd.DataFrame({'caption':np.array(corpus), 'num_words': full_df['number_of_words'].values})
 y = full_df['number_of_likes'].values
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
 
 #%%
-#Create Tf-idf vectorizer and save feature names, vocabulary set, and ignored words
+#Create Tf-idf vectorizer, transform data, 
+# combine tf-idf vector and number of words to feed into model 
 vector_train = TfidfVectorizer(min_df= 0.00017)
 X_vector = vector_train.fit_transform(Xtrain['caption'])
 X_train = np.array(X_vector.todense())
 x_full_train = np.concatenate((X_train, Xtrain['num_words'].values.reshape(-1,1)), axis=1)
-
-popular_words = vector_train.get_feature_names()
 
 vocab = vector_train.vocabulary_
 ignored_words = vector_train.stop_words_
@@ -113,19 +127,3 @@ rf.fit(x_full_train, ytrain)
 print("Random Forest score:", rf.score(x_full_test, ytest))
 
 #n_estimators = 10, max_features=0.33 = score -0.48
-
-#%%
-#Graph relationship between number of likes and number of words
-likes = full_df['number_of_likes'].values
-words = full_df['number_of_words'].values
-
-words_likes = hv.Scatter((words, likes)).opts(xlabel='Number of words in caption', ylabel='Number of likes',title='Words vs. Likes')
-words_likes.redim(x=hv.Dimension('x', range=(0, 100)), y=hv.Dimension('y', range=(0,100)))
-#%%
-#Linear Regression Model
-linear = LinearRegression()
-linear.fit(x_full_train, ytrain)
-print("Linear Regression score:", linear.score(x_full_test, ytest))
-
-
-#%%
