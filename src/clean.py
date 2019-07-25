138a5dc318a204af6bb43a13f19aab17e300cb95
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-25 18:54:42 -0500

    got a score of 0.61!!

--- src/clean.py
+++ src/clean.py
@@ -30,12 +30,12 @@ Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
 #%%
 #Create Tf-idf vectorizer, transform data, 
 # combine tf-idf vector and number of words to feed into model 
-vector_train = TfidfVectorizer(min_df= 0.00017)
+vector_train = TfidfVectorizer(min_df= 0.005)
 caption_vector = vector_train.fit_transform(Xtrain['caption'])
 vocab = vector_train.vocabulary_
 
-vec_df = pd.DataFrame(caption_vector.todense())
-no_caption = Xtrain.iloc[:,1:]
+vec_df = pd.DataFrame(caption_vector.todense(), index=Xtrain['caption'].index)
+no_caption = Xtrain.iloc[:,2:]
 train_x = vec_df.join(no_caption)
 
 
@@ -43,12 +43,14 @@ train_x = vec_df.join(no_caption)
 #Create Xtest vector from Xtrain vocabulary
 vector_test = TfidfVectorizer(vocabulary=vocab)
 caption_test_vector = vector_test.fit_transform(Xtest['caption'])
-vec_test_df = pd.DataFrame(caption_test_vector.todense())
-no_caption_test = Xtest.iloc[:,1:]
+vec_test_df = pd.DataFrame(caption_test_vector.todense(), index=Xtest['caption'].index)
+no_caption_test = Xtest.iloc[:,2:]
 test_x = vec_test_df.join(no_caption_test)
 
 #%%
 #Random Forest Regression Model
-rf = RandomForestRegressor(n_estimators = 10, max_features=0.33, n_jobs=-1)
+rf = RandomForestRegressor(n_estimators = 50, max_features=0.33, n_jobs=-1)
 rf.fit(train_x, ytrain)
 print("Random Forest score:", rf.score(test_x, ytest))
+
+'''Score of .61!!!!'''
