b12dd2f571f75ad1de93fec58e6dc2c5a19249c6
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-26 15:13:34 -0500

    top words func added

--- src/transforming.py
+++ src/transforming.py
@@ -6,6 +6,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, punkt
 from nltk.stem import PorterStemmer
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 import nltk
 nltk.download('punkt')
 
@@ -134,3 +136,23 @@ def tokenize_corpus(corpus):
         
         return corpus_two
 
+def get_top_n_words(corpus, n=None):
+    """
+    List the top n words in a vocabulary according to occurrence in a text corpus.
+    
+    get_top_n_words(["I love Python", "Python is a language programming", "Hello world", "I love the world"]) -> 
+    [('python', 2),
+     ('world', 2),
+     ('love', 2),
+     ('hello', 1),
+     ('is', 1),
+     ('programming', 1),
+     ('the', 1),
+     ('language', 1)]
+    """
+    vec = CountVectorizer().fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
