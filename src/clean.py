d386ba69cfcef25020b4a0324bf74b5c4ceecad7
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-25 20:20:12 -0500

    new scores with user dummies

--- src/clean.py
+++ src/clean.py
@@ -22,9 +22,13 @@ df.head()
 totals = pd.read_csv('./user_totals.csv', header=0, names = ['user', 'user_posts', 'user_followers'])
 complete_df = df.merge(totals, how = 'left', on='user')
 
+user_dummies = pd.get_dummies(complete_df['user'])
+new_df = pd.concat((complete_df, user_dummies), axis=1).drop(columns='user')
+
+new_df.head()
 #%%
-X = complete_df.iloc[:,1:]
-y = complete_df.iloc[:,0].values
+X = new_df.iloc[:,1:]
+y = new_df.iloc[:,0].values
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
 
 #%%
@@ -35,7 +39,7 @@ caption_vector = vector_train.fit_transform(Xtrain['caption'])
 vocab = vector_train.vocabulary_
 
 vec_df = pd.DataFrame(caption_vector.todense(), index=Xtrain['caption'].index)
-no_caption = Xtrain.iloc[:,2:]
+no_caption = Xtrain.iloc[:,1:]
 train_x = vec_df.join(no_caption)
 
 
@@ -44,7 +48,7 @@ train_x = vec_df.join(no_caption)
 vector_test = TfidfVectorizer(vocabulary=vocab)
 caption_test_vector = vector_test.fit_transform(Xtest['caption'])
 vec_test_df = pd.DataFrame(caption_test_vector.todense(), index=Xtest['caption'].index)
-no_caption_test = Xtest.iloc[:,2:]
+no_caption_test = Xtest.iloc[:,1:]
 test_x = vec_test_df.join(no_caption_test)
 
 #%%
@@ -53,4 +57,8 @@ rf = RandomForestRegressor(n_estimators = 50, max_features=0.33, n_jobs=-1)
 rf.fit(train_x, ytrain)
 print("Random Forest score:", rf.score(test_x, ytest))
 
-'''Score of .61!!!!'''
+
+#%%
+'''Score of .61 with min_df=0.005, 10 trees, no user information
+    Score of .535 with min_df=0.005, 10 trees and user information
+    Score of .545 with min_df=0.005, 50 trees and user information '''
