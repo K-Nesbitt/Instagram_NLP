991294e0c51140d26d9b9f1107ee1526fe511a17
Keatra Nesbitt <keatralynn@gmail.com>
2019-07-26 10:41:07 -0500

    new classifier scores

--- src/clean.py
+++ src/clean.py
@@ -1,43 +1,31 @@
 #%%
-from src.transforming import create_full_df, create_corpus, tokenize_corpus
+from src.transforming import create_full_df
 
 from sklearn.feature_extraction.text import TfidfVectorizer 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-
-from nltk.tokenize import word_tokenize, punkt
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
 import pandas as pd 
 import numpy as np
-from collections import Counter
-import operator
+
 
 #%%
 data_path = '/Users/keatra/Galvanize/Projects/Instagram_likes_nlp/data_2'
-df = create_full_df(data_path)
-df.head()
+data = create_full_df(data_path)
+data.head()
 
 #%%
-#Join dataframe with features and user totals
-totals = pd.read_csv('./user_totals.csv', header=0, names = ['user', 'user_posts', 'user_followers'])
-complete_df = df.merge(totals, how = 'left', on='user')
 
-user_dummies = pd.get_dummies(complete_df['user'])
-new_df = pd.concat((complete_df, user_dummies), axis=1).drop(columns='user')
+target = data['number_of_likes'].apply(lambda x: 0 if x <= data.user_avg_likes[x] else 1)
 
-new_df.head()
-#%%
-mu = round(new_df['number_of_likes'].mean(), 0)
-cls_target = new_df['number_of_likes'].apply(lambda x: 0 if x <= mu else 1)
-
-X = new_df.iloc[:,1:]
-y = cls_target.values
+X = data.iloc[:, 1:]
+y = target.values
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
 
 #%%
 #Create Tf-idf vectorizer, transform data, 
 # combine tf-idf vector and number of words to feed into model 
-vector_train = TfidfVectorizer(min_df= 0.0025)
+vector_train = TfidfVectorizer(min_df= 0.0010)
 caption_vector = vector_train.fit_transform(Xtrain['caption'])
 vocab = vector_train.vocabulary_
 
@@ -70,7 +58,6 @@ print("Random Forest score:", rf.score(test_x, ytest))
     Score of .601 with min_df=0.0025, 25 trees, user information '''
 
 #%%
-from sklearn.ensemble import RandomForestClassifier
 
 rfc = RandomForestClassifier(n_estimators=25, n_jobs=-1)
 rfc.fit(train_x, ytrain)
@@ -79,5 +66,9 @@ print("Random Forest Classifier score:", rfc.score(test_x, ytest))
 
 '''Classifier score of .891 with 25 trees
     only 25% of the data is over the mean therefore it is easier
-    to predict'''
+    to predict
+    
+    Classifer score of .901 with 25 trees and min_df .0025, 
+    score of .908 with min_df = 0.005
+    score of .907 with min_df = 0.0010'''
 #%%
